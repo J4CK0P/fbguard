6aa3e677882c9e3356049ea87e00dca80c3f9884
J4CKOP <53121879+J4CK0P@users.noreply.github.com>
2019-07-27 15:57:07 +0700

    Create guard.sh

---
+++ guard.sh
@@ -0,0 +1,85 @@
+#!/system/xbin/bash
+clear
+echo "Tunggu lagi Colii"
+git clone https://github.com/Noxturnix/guardn
+git clone https://github.com/siputra12/sholatoy
+
+toilet -f slant --gay "Toolsv¹"
+sleep 1
+echo "\033[33;1m tool untuk pp guard & alarm adzan "
+sleep 1
+echo "\033[36;1m★""\033[34;1mSANTUY HACKER TEAM""\033[36;1m★"
+sleep 1
+echo "\033[35;1m1.""\033[36;1mPP GUARD"
+echo "\033[35;1m2.""\033[36;1mALARM"
+
+echo "\033[37;1m3.""\033[30;1mInformasi Tool Ini"
+echo "\033[31;1m0. Keluar"
+echo "\033[33;1m Pilih Angka:"
+
+read mrrm
+if [ $mrrm = 1 ] || [ $mrrm = 1 ]
+then
+clear
+echo "\033[36;1m"
+figlet "Guard PP Fb"
+echo "\033[31;1mPemograman:""\033[37;1mPYTHON2"
+echo "\033[31;1mAuthor:""\033[37;1mJ4CKOP"
+sleep 5
+clear
+cd $HOME/fbguard/Noxturnix/guardn
+python2 guardn.py
+fi
+
+if [ $mrrm = 2 ] || [ $mrrm = 2 ]
+then
+clear
+echo "\033[36;1m"
+figlet "ALARM ADZAN"
+echo "\033[31;1mPemograman:""\033[37;1mPHP"
+echo "\033[31;1mAuthor:""\033[37;1mJ4CKOP"
+sleep 5
+clear
+cd $HOME/fbguard/siputra12/sholatoy
+php run.php
+fi
+
+if
+[ $mrrm = 3 ] || [ $mrrm = 3 ]
+then
+clear
+toilet -f slant --gay "TOOLS NEW"
+echo "\033[34;1mContact:""\033[37;1m https://bit.ly/2yarlam "
+
+
+sleep 1
+echo "\033[35;1mPemograman:""\033[36;1m Bash"
+
+echo "\033[33;1mKarya: SHT"
+sleep 1
+echo "\033[32;1mVersi: v1"
+sleep 1
+echo "\033[36;1mTeam: SANTUY HACKER TEAM"
+sleep 1
+echo "\033[31;1mSubscribe YouTube""\033[37;1m https://www.youtube.com/channel/UCn82YGg7CnKcZh7QaNkLT8Q"
+echo "\033[35;1m Spesial Thanks To: "
+echo "\033[36;1m"
+echo "SANTUY HACKER TEAM"
+sleep 1
+echo "HACKER INDONESIA"
+sleep 1
+
+echo "\033[30;1m tunggu 8 detik"
+sleep 8
+cd $HOME/bomcall/
+sh bomcall.sh
+fi
+
+if
+[ $mrrm = 0 ] || [ $mrrm = 0 ]
+then
+echo "\033[31;1m Keluar"
+sleep 1
+echo "\033[32;1m Sampai berjumpa lagi :)"
+sleep 1
+fi
